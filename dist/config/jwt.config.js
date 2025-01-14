9da8419599c2ad3c98ca67cfb54921347bffc617
Messaoud Hamdi <89160125+hmdmsd@users.noreply.github.com>
2025-01-14 23:19:39 +0100

    Delete dist directory

--- dist/config/jwt.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtConfig = void 0;
-exports.jwtConfig = {
-    secret: process.env.JWT_SECRET || 'default_secret', // Replace with a secure secret
-    expiresIn: '24h', // Token expiration time
-    algorithm: 'HS256', // Hashing algorithm
-};
